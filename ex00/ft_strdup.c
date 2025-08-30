f538d8586eb75867af31644767a7d477ad5f66c2
betty <ibtissambassoma84@gmail.com>
2025-08-30 22:19:44 +0100

    strdup

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*dup;
+	int		i;
+
+	len = ft_strlen(src) + 1;
+	dup = malloc(sizeof(char) * len);
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+// int main() {
+//     char *duplicate = ft_strdup("Hello World");
+//     free(duplicate);
+// }
